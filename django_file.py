6e54b96540bba516d6c17142b19462c725046396
Abhilash-Singh-Thakur <57857463+Abhilash-Singh-Thakur@users.noreply.github.com>
2023-04-29 17:02:26 +0530

    Create django_file.py

---
+++ django_file.py
@@ -0,0 +1,13 @@
+pip install virtualenv
+
+pip install django 
+
+pip install django_project_name
+
+python manage.py runserver   #To run the server by default on 8000
+
+python manage.py runserver 9000  # to run the server on 9000
+
+cd folder/manage.py runserver   # we can run the server by this command also.
+
+python manage.py startapp courses  # this will create the app courses
